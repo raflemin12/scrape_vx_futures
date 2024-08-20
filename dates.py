0b69d690b723b3398ea58a941950558a061239d0
Ricky Fleming <raflemin@gmail.com>
2024-08-20 12:07:20 -0700

    format document

--- dates.py
+++ dates.py
@@ -10,7 +10,8 @@ third_wed = []
 
 for year in years:
     for month in months:
-        third_wed.append([day.strftime('%Y-%m-%d') for week in c.monthdatescalendar(year,month) for day in week \
-                         if day.weekday() == calendar.WEDNESDAY and day.month == month][2])
+        third_wed.append([day.strftime('%Y-%m-%d') for week in c.monthdatescalendar(year,month) \
+                          for day in week if day.weekday() == calendar.WEDNESDAY \
+                          and day.month == month][2])
 
 print(third_wed)
