df4d2139f5da7faa260c351c8a0c88ce1952df8a
Ricky Fleming <raflemin@gmail.com>
2024-08-19 17:11:35 -0700

    for loop gets 3rd wed every month from years

--- dates.py
+++ dates.py
@@ -3,5 +3,13 @@ import calendar
 years = [year for year in range(2013, 2026)]
 months = [month for month in range(1,13)]
 
-print(years)
-print(months)
+c = calendar.Calendar()
+
+third_wed = []
+
+for year in years:
+    for month in months:
+        third_wed.append([day for week in c.monthdatescalendar(year,month) for day in week \
+                         if day.weekday() == calendar.WEDNESDAY and day.month == month][2])
+
+print(third_wed)
