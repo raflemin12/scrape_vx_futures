0d91736e0e1eb2efcfb8f44e00c9e4ee09b11d52
Ricky Fleming <raflemin@gmail.com>
2024-08-19 17:15:57 -0700

    convert dates to string in for loop

--- dates.py
+++ dates.py
@@ -1,4 +1,5 @@
 import calendar
+# from datetime import strftime
 
 years = [year for year in range(2013, 2026)]
 months = [month for month in range(1,13)]
@@ -9,7 +10,7 @@ third_wed = []
 
 for year in years:
     for month in months:
-        third_wed.append([day for week in c.monthdatescalendar(year,month) for day in week \
+        third_wed.append([day.strftime('%Y-%m-%d') for week in c.monthdatescalendar(year,month) for day in week \
                          if day.weekday() == calendar.WEDNESDAY and day.month == month][2])
 
 print(third_wed)
