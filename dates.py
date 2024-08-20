b635c5cd1a6fa96c8556684ccd49da61bc82e44b
Ricky Fleming <raflemin@gmail.com>
2024-08-20 14:10:45 -0700

    turned program into a callable function

--- dates.py
+++ dates.py
@@ -1,17 +1,17 @@
 import calendar
-# from datetime import strftime
 
-years = list(range(2013, 2026))
-months = list(range(1,13))
+def third_wed_month(start_year: int, end_year: int) -> list:
 
-c = calendar.Calendar()
+    calendar_obj = calendar.Calendar()
 
-third_wed = []
+    third_wed = []
 
-for year in years:
-    for month in months:
-        third_wed.append([day.strftime('%Y-%m-%d') for week in c.monthdatescalendar(year,month) \
+    for year in range(start_year, end_year + 1):
+        for month in range(1,13):
+            third_wed.append([day.strftime('%Y-%m-%d') \
+                          for week in calendar_obj.monthdatescalendar(year,month) \
                           for day in week if day.weekday() == calendar.WEDNESDAY \
                           and day.month == month][2])
+    return third_wed
 
-print(third_wed)
+print(third_wed_month(2013, 2025))
