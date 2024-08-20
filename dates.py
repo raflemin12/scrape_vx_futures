f766ce2305f8c1e041008ba823b7d7e2f41e53ea
Ricky Fleming <raflemin@gmail.com>
2024-08-20 13:00:57 -0700

    Format code

--- dates.py
+++ dates.py
@@ -1,8 +1,8 @@
 import calendar
 # from datetime import strftime
 
-years = [year for year in range(2013, 2026)]
-months = [month for month in range(1,13)]
+years = list(range(2013, 2026))
+months = list(range(1,13))
 
 c = calendar.Calendar()
 
