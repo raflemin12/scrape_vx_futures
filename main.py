4576c943b847277494080ef84e5cd6098d50d038
Ricky Fleming <raflemin@gmail.com>
2024-08-21 16:00:21 -0700

    create url string to format

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from dates import third_wed_month
 
+url = 'https://cdn.cboe.com/data/us/futures/market_statistics/historical_data/VX/VX_{date}.csv'
+
 df = pd.read_csv('https://cdn.cboe.com/data/us/futures/market_statistics/historical_data/VX/VX_2024-08-21.csv')
 
 print(df)
