398fb1adcf79f090ff68a85822fd71ad7244fac2
Ricky Fleming <raflemin@gmail.com>
2024-08-21 16:23:31 -0700

    create loop to download data store in dict

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import pandas as pd
+import time
 from dates import third_wed_month
 
-URL = 'https://cdn.cboe.com/data/us/futures/market_statistics/historical_data/VX/VX_{date}.csv'
+URL = 'https://cdn.cboe.com/data/us/futures/market_statistics/historical_data/VX/VX_{date_str}.csv'
 
-df = pd.read_csv(URL.format(date='2024-08-21'))
+df = pd.read_csv(URL.format(date_str='2024-08-21'))
 
-print(df)
+futures_dict = {}
+
+for date in third_wed_month(2013, 2025):
+    try:
+        df = pd.read_csv(URL.format(date_str=date))
+        futures_dict[date] = df
+        time.sleep(3)
+    except:
+        print(date)
+
+print(futures_dict)
