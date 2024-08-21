fee7fa3787e6317d36d4865aae11969033d8347d
Ricky Fleming <raflemin@gmail.com>
2024-08-21 16:03:28 -0700

    test url works with csv get

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from dates import third_wed_month
 
-url = 'https://cdn.cboe.com/data/us/futures/market_statistics/historical_data/VX/VX_{date}.csv'
+URL = 'https://cdn.cboe.com/data/us/futures/market_statistics/historical_data/VX/VX_{date}.csv'
 
-df = pd.read_csv('https://cdn.cboe.com/data/us/futures/market_statistics/historical_data/VX/VX_2024-08-21.csv')
+df = pd.read_csv(URL.format(date='2024-08-21'))
 
 print(df)
