5f6db4e0ba403c7770940007bf426c1359b9cc02
Ricky Fleming <raflemin@gmail.com>
2024-08-22 15:46:29 -0700

    list of css ids

--- scrape_dates.py
+++ scrape_dates.py
@@ -2,3 +2,7 @@ import urllib
 from bs4 import BeautifulSoup
 
 URL = 'https://www.macroption.com/vix-expiration-calendar/#history'
+
+css_id_list = ['2013', '2014', '2015', '2016'
+               '2017', '2018', '2019', '2020'
+               '2021', '2022', '2023', '2024', '2025']
