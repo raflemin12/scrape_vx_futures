8b59d86ee70f523d573c2b33779a6cf19c4b8b55
Ricky Fleming <raflemin@gmail.com>
2024-08-27 13:47:31 -0700

    get page html

--- scrape_dates.py
+++ scrape_dates.py
@@ -1,4 +1,4 @@
-import urllib
+import requests
 from bs4 import BeautifulSoup
 
 URL = 'https://www.macroption.com/vix-expiration-calendar/#history'
@@ -6,3 +6,7 @@ URL = 'https://www.macroption.com/vix-expiration-calendar/#history'
 css_id_list = ['2013', '2014', '2015', '2016'
                '2017', '2018', '2019', '2020'
                '2021', '2022', '2023', '2024', '2025']
+r = requests.get(URL)
+soup = BeautifulSoup(r.content, 'html.parser')
+
+print(soup)
