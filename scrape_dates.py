8712553aa16e297af054f311457451e908334ee7
Ricky Fleming <raflemin@gmail.com>
2024-08-28 12:36:08 -0700

    create list of date strings and month_to_num dict

--- scrape_dates.py
+++ scrape_dates.py
@@ -6,8 +6,23 @@ URL = 'https://www.macroption.com/vix-expiration-calendar/#history'
 css_id_list = ['2013', '2014', '2015', '2016'
                '2017', '2018', '2019', '2020'
                '2021', '2022', '2023', '2024', '2025']
+
+month_to_num = {
+    'january': '01'
+    ,'february': '02'
+    ,'march': '03'
+    ,'april':'04'
+    ,'may': '05'
+    ,'june': '06'
+    ,'july': '07'
+    ,'august': '08'
+    ,'september': '09'
+    ,'october': '10'
+    ,'november': '11'
+    ,'december': '12'
+}
 r = requests.get(URL)
 soup = BeautifulSoup(r.content, 'html.parser')
 paragraph = soup.find('h3', {'id': '2013'}).find_next('p')
-print(type(paragraph))
-print(list(paragraph))
+
+print([date.strip().replace(' ', '-').lower() for date in list(paragraph) if isinstance(date, str)])
