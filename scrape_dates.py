c8672bbb3cc7b074ae9f5b704dbfd70c95cb434b
Ricky Fleming <raflemin@gmail.com>
2024-08-27 17:59:57 -0700

    Find h3 next p efficient

--- scrape_dates.py
+++ scrape_dates.py
@@ -8,5 +8,4 @@ css_id_list = ['2013', '2014', '2015', '2016'
                '2021', '2022', '2023', '2024', '2025']
 r = requests.get(URL)
 soup = BeautifulSoup(r.content, 'html.parser')
-
-print(soup.find('h3', {'id':'2013'}).next_sibling.next_sibling)
+paragraph = soup.find('h3', {'id': '2013'}).find_next('p')
