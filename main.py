bbe1ae13e20c17c1095e9e525c4b26a43e2b8e13
Ricky Fleming <raflemin@gmail.com>
2024-09-18 14:07:33 -0700

    remove libraries and uncomment URL

--- main.py
+++ main.py
@@ -1,25 +1,11 @@
 import pandas as pd
-import time
-from dates import third_wed_month
 from scrape_dates import HistExp
 
-"""
+
 URL = 'https://cdn.cboe.com/data/us/futures/market_statistics/historical_data/VX/VX_{date_str}.csv'
 
-df = pd.read_csv(URL.format(date_str='2024-08-21'))
+# df = pd.read_csv(URL.format(date_str='2024-08-21'))
 
-futures_dict = {}
-
-for date in third_wed_month(2013, 2025):
-    try:
-        df = pd.read_csv(URL.format(date_str=date))
-        futures_dict[date] = df
-        time.sleep(3)
-    except:
-        print(date)
-
-print(futures_dict)
-"""
 
 
 def main():
