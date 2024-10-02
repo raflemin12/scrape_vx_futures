a6e5ab4ce344da2b554b164093a8f727eab117de
Ricky Fleming <raflemin@gmail.com>
2024-10-02 12:29:09 -0700

    edit main for WebHTML class

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from scrape_dates import HistExp
+from scrape_dates import ExpDates, WebHTML
 
 
 URL = 'https://cdn.cboe.com/data/us/futures/market_statistics/historical_data/VX/VX_{date_str}.csv'
@@ -9,7 +9,8 @@ URL = 'https://cdn.cboe.com/data/us/futures/market_statistics/historical_data/VX
 
 
 def main():
-    hist_exp = HistExp(2013, 2026)
+    expiration_html = WebHTML('https://www.macroption.com/vix-expiration-calendar/#history')
+    hist_exp = ExpDates(2013, 2026, expiration_html.get_website())
     exp_dates = hist_exp.get_exp_dates()
     print(exp_dates)
 
