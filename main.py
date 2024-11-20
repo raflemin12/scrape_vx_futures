d312480125d80fc03c2ad6699c84b1f284558423
Ricky <raflemin@gmail.com>
2024-11-19 17:15:09 -0800

    remove inplace replacement on df

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ URL = 'https://cdn.cboe.com/data/us/futures/market_statistics/historical_data/VX
 def main():
     expiration_html = WebHTML('https://www.macroption.com/vix-expiration-calendar/#history')
     hist_exp = ExpDates(2013, 2026, expiration_html.get_website())
-    exp_dates = hist_exp.get_exp_dates()
+    hist_exp.get_exp_dates()
     df_list = []
-    for date in exp_dates:
+    for date in hist_exp.get_date_strings():
         try:
             df = pd.read_csv(URL.format(date_str=date))
             df_list.append(df)
@@ -21,9 +21,9 @@ def main():
             print(date)
     large_df = pd.concat(df_list)
     future_expiration = list(large_df['Futures'].unique())
-    slice_exp_dates = exp_dates[:len(future_expiration)+1]
-    exp_date_dict = dict(zip(future_expiration, slice_exp_dates))
-    large_df['Futures'].replace(exp_date_dict, inplace=True)
+    slice_exp_dates = hist_exp.get_date_strings()[:len(future_expiration)]
+    large_df['Futures'] = large_df['Futures'].replace(
+        to_replace = future_expiration, value = slice_exp_dates)
     print(large_df)
 
 if __name__ == "__main__":
